45a9c5be1b639fb7b959dc316f644f7e19f8292c
Marco Nolletti <mnolletti@gmail.com>
2017-01-31 23:26:24 +0100

    Code cleanup

--- script.js
+++ script.js
@@ -10,45 +10,6 @@
 /* DOKUWIKI:include_once lightGallery/js/jquery.mousewheel.min.js */
 		
 /**
- * Add a quicklink to the media popup
- */
-/*function gallery_plugin(){
-    var $opts = jQuery('#media__opts');
-    if(!$opts.length) return;
-    if(!window.opener) return;
-
-    var glbl = document.createElement('label');
-    var glnk = document.createElement('a');
-    var gbrk = document.createElement('br');
-    glnk.name         = 'gallery_plugin';
-    glnk.innerHTML    = LANG.plugins.gallery.addgal; //FIXME localize
-    glnk.style.cursor = 'pointer';
-
-    glnk.onclick = function(){
-        var $h1 = jQuery('#media__ns');
-        if(!$h1.length) return;
-        var ns = $h1[0].innerHTML;
-        opener.insertAtCarret('wiki__text','{{gallery>'+ns+'}}');
-        if(!dw_mediamanager.keepopen) window.close();
-    };
-
-    $opts[0].appendChild(glbl);
-    glbl.appendChild(glnk);
-    $opts[0].appendChild(gbrk);
-}*/
-
-/**
- * Display a selected page and hide all others
- */
-/*function gallery_pageselect(e){
-    var galid = e.target.hash.substr(10,4);
-    var $pages = jQuery('div.gallery__'+galid);
-    $pages.hide();
-    jQuery('#'+e.target.hash.substr(1)).show();
-    return false;
-}*/
-
-/**
  * Initialize lightGallery for pg-show class
  */
 function InitPgGallery(tw,th){
@@ -69,6 +30,7 @@ function InitPgGallery(tw,th){
 
 /**
  * Attach click event to the poster <a> tag
+   Runs when the DOM is ready
  */
 jQuery(document).on('click', 'a.pg-start', function() {
 	var $pgid = '#' + jQuery(this).data('pg-id');
@@ -79,6 +41,10 @@ jQuery(document).on('click', 'a.pg-start', function() {
 	return false;
 });
 
+/**
+ * Attach click event to the poster <a> tag
+	 Runs when complete page is fully loaded, including all frames, objects and images
+ */
 jQuery(window).load(function(){
 	// Trigger thumbnails preload
 	jQuery('img.pg-preload').each(function(index) {
@@ -92,17 +58,5 @@ jQuery(window).load(function(){
 // //					jQuery(this).attr('src', jQuery(this).data('src'));
 	// });
 });
-
-// initialize the lightbox mechanism
-// jQuery("a.lightbox, a[rel^='lightbox']").swipebox({
-		// loopAtEnd: true
-// });
-
-// gallery_plugin();
-
-// hide all pages except the first one
-// var $pages = jQuery('div.gallery_page');
-// $pages.hide();
-// $pages.eq(0).show();
 
 //		jQuery("img.lazy").lazyload();
