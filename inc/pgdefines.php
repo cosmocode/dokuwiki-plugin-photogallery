08173e19e0dc22bd63e508b98dba10f48307f016
Marco Nolletti <mnolletti@gmail.com>
2017-04-01 10:35:28 +0200

    Started development of pgFetch.php

--- inc/pgdefines.php
+++ inc/pgdefines.php
@@ -9,7 +9,7 @@
 if(!defined('DOKU_INC')) define('DOKU_INC',realpath(__DIR__.'/../../').'/');
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 if(!defined('PHOTOGALLERY_REL')) define('PHOTOGALLERY_REL',DOKU_REL.'lib/plugins/photogallery/');
-if(!defined('PHOTOGALLERY_PGIMG')) define('PHOTOGALLERY_PGIMG','phpThumb/pgImg.php');
+if(!defined('PHOTOGALLERY_PGIMG')) define('PHOTOGALLERY_PGIMG','phpThumb/pgFetch.php');
 if(!defined('PHOTOGALLERY_PGIMG_REL')) define('PHOTOGALLERY_PGIMG_REL',PHOTOGALLERY_REL.PHOTOGALLERY_PGIMG);
 if(!defined('PHOTOGALLERY_PGIMG_FILE')) define('PHOTOGALLERY_PGIMG_FILE',realpath(__DIR__.'/../'.PHOTOGALLERY_PGIMG));
 if(!defined('PHOTOGALLERY_MEDIA_REL')) define('PHOTOGALLERY_MEDIA_REL',DOKU_REL.$GLOBALS['conf']['savedir'].'/media/');
